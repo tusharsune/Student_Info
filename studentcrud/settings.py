4fedb7aeb9f602bfb72497493e5e4ddbc7a9e0d4
Tushar Sune <147902935+tusharsune@users.noreply.github.com>
2025-05-10 20:49:32 +0530

    Update settings.py

--- studentcrud/settings.py
+++ studentcrud/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'c1v)(20r^-190@cfbjq@d7m65z=(5f#gq@ocw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = ['student-info-eg0b.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
