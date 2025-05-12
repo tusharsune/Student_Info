094c790ab8017d8c77e1894b269b98c21836b679
Tushar Sune <147902935+tusharsune@users.noreply.github.com>
2025-05-12 17:16:41 +0530

    Update settings.py

--- studentcrud/settings.py
+++ studentcrud/settings.py
@@ -1,19 +1,17 @@
 from pathlib import Path
 import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+LOGIN_REDIRECT_URL = '/admin/'
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', 'c1v)(20r^-190@cfbjq@d7m65z=(5f#gq@ocwb8j8+ol+78u2)')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 # ALLOWED_HOSTS = ['student-info-eg0b.onrender.com', 'localhost', '127.0.0.1']
 ALLOWED_HOSTS = ['*']
 
-# Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -27,7 +25,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # MUST be right after SecurityMiddleware
+    'whitenoise.middleware.WhiteNoiseMiddleware',  
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -55,7 +53,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'studentcrud.wsgi.application'
 
-# Database (SQLite for development)
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -63,7 +60,6 @@ DATABASES = {
     }
 }
 
-# Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -79,23 +75,17 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# Internationalization
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 
-# All static files in app folders (development use)
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'studentapp/static')]
 
-# Where collectstatic will place files for production
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Required for WhiteNoise to serve compressed static files
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
