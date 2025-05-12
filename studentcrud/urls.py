024474f9f4bd8b43a199d0728877ff0b16178065
Tushar Sune <147902935+tusharsune@users.noreply.github.com>
2025-05-12 17:31:49 +0530

    Update urls.py

--- studentcrud/urls.py
+++ studentcrud/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
+from studentapp.views import create_admin_user
+
+create_admin_user()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
