339a13d1017d0d4868fbcc057c3b2f235d474436
Tushar Sune <147902935+tusharsune@users.noreply.github.com>
2025-05-12 17:17:27 +0530

    Update admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+User = get_user_model()
+
+admin.site.unregister(User)
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_active', 'date_joined')
+    search_fields = ('username', 'email')
+    list_filter = ('is_active', 'is_staff', 'is_superuser')
+    actions = ['deactivate_users']
+
+    def deactivate_users(self, request, queryset):
+        """Custom action to deactivate selected users"""
+        queryset.update(is_active=False)
+        self.message_user(request, "Successfully deactivated users.")
+
+    deactivate_users.short_description = "Deactivate selected users"
+
+admin.site.register(User, CustomUserAdmin)
